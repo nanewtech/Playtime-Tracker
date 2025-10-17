6ab56078e4f9f291ed6e25f5e5451de37817274d
nanewtech <itznanewtech@gmail.com>
2025-10-17 02:47:15 +0200

    positioned button

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace geode::prelude;
 
 
 #include <Geode/modify/LevelInfoLayer.hpp>
-class $modify(MyLevelInfoLayer, LevelInfoLayer) {
+class $modify(ptLevelInfoLayer, LevelInfoLayer) {
 
 
 
@@ -22,24 +22,25 @@ class $modify(MyLevelInfoLayer, LevelInfoLayer) {
 
 
 
-		auto myButton = CCMenuItemSpriteExtra::create(
+		auto playtimeButton = CCMenuItemSpriteExtra::create(
 			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
 			this,
-			menu_selector(MyLevelInfoLayer::onMyButton)
+			menu_selector(ptLevelInfoLayer::onPlaytimeButton)
 		);
 
 		auto menu = this->getChildByID("other-menu");
-		menu->addChild(myButton);
+		menu->addChild(playtimeButton);
 
-		myButton->setID("my-button"_spr);
-		myButton->setContentSize({ 46.f, 47.5f });
-		menu->ignoreAnchorPointForPosition(false);
+		playtimeButton->setID("playtime-tracker-button");
+		playtimeButton->setPosition({ 38.f, 76.5f });
+		playtimeButton->setScale(0.8f);
+
 		menu->updateLayout();
 
 		return true;
 	}
 
-	void onMyButton(CCObject*) {
+	void onPlaytimeButton(CCObject*) {
 		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
 	}
 };
